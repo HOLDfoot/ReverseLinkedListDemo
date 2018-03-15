c6580a4442e4d81c88c1d8c926c5149120c25de7
ZhuMingren <zhumingren@secretlisa.com>
2018-03-15 23:05:07 +0800

    Add完成链表

--- app/src/main/cpp/linkedlist.c
+++ app/src/main/cpp/linkedlist.c
@@ -4,6 +4,7 @@
 #include "linkedlist.h"
 #include <jni.h>
 #include "mylog.h"
+#include <malloc.h>
 
 void testLinkedList() {
     LOGD("testLinkedList");
@@ -13,25 +14,44 @@ void testLinkedList() {
         struct Node *next;
     };
 
-    struct Node node = {
+    struct Node node0 = {
             .id = 0,
             .next = NULL
     };
-    struct Node *head = &node;
+    struct Node *head = &node0;
     struct Node *tempNext = head;
     for (int i = 1; i < 4; i++) {
-        struct Node node = {
-                .id = i,
-                .next = NULL
-        };
+        struct Node *node = (struct Node*) malloc(sizeof(struct Node));
+        node->id = i;
+        node->next = NULL;
+        LOGD("node init i = %d", node->id);
+        if (node->next != NULL) {
+            LOGE("node node = %d", node->id);
+        } else {
+            LOGE("node node->next = null");
+        }
 
-        struct Node *next = &node;
+        tempNext->next = node;
+        if (node->next != NULL) {
+            LOGD("node node = %d", node->id);
+            LOGE("node node.next->id = %d", node->next->id);
+        } else {
+            LOGD("node node->next = null");
+        }
 
-        tempNext->next = next;
+        tempNext = node;
 
-        tempNext = next;
+        if (tempNext->next != NULL) {
+            LOGD("node tempNext = %d", tempNext->id);
+        } else {
+            LOGD("node tempNext->next = null");
+        }
+        LOGE("node get i = %d", node->id);
     }
 
-
-
+    struct Node *tempPoint = head;
+    while (tempPoint != NULL) {
+        LOGD("tempPoint node i = %d", tempPoint->id);
+        tempPoint = tempPoint->next;
+    }
 }
