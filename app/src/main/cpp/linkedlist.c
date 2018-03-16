74672fb387c4f064cb5a77884b361e1807928c77
ZhuMingren <zhumingren@secretlisa.com>
2018-03-16 10:58:10 +0800

    Add完成链表的reverse

--- app/src/main/cpp/linkedlist.c
+++ app/src/main/cpp/linkedlist.c
@@ -12,6 +12,9 @@ struct Node
     struct Node *next;
 };
 
+void printLinkedList(struct Node *tempPoint);
+struct Node* reverseLikedList(struct Node *head);
+
 void testLinkedList() {
     LOGD("testLinkedList");
 
@@ -25,45 +28,80 @@ void testLinkedList() {
         struct Node *node = (struct Node*) malloc(sizeof(struct Node));
         node->id = i;
         node->next = NULL;
-        LOGD("node init i = %d", node->id);
-        if (node->next != NULL) {
-            LOGE("node node = %d", node->id);
-        } else {
-            LOGE("node node->next = null");
-        }
 
         tempNext->next = node;
-        if (node->next != NULL) {
-            LOGD("node node = %d", node->id);
-            LOGE("node node.next->id = %d", node->next->id);
-        } else {
-            LOGD("node node->next = null");
-        }
 
         tempNext = node;
-
-        if (tempNext->next != NULL) {
-            LOGD("node tempNext = %d", tempNext->id);
-        } else {
-            LOGD("node tempNext->next = null");
-        }
-        LOGE("node get i = %d", node->id);
     }
 
-    struct Node *tempPoint = head;
+    printLinkedList(head);
+
+    struct Node *newHead = reverseLikedList(head);
+    printLinkedList(newHead);
+}
+
+void printLinkedList(struct Node *tempPoint) {
     while (tempPoint != NULL) {
         LOGD("tempPoint node i = %d", tempPoint->id);
         tempPoint = tempPoint->next;
     }
-
-
-
 }
 
 struct Node* reverseLikedList(struct Node *head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    struct Node *pre, *cur, *ne;
+    pre = head;
+    cur = head->next;
+    while (cur) {
+        ne = cur->next; //保存会更改的变量
+
+        cur->next = pre; //更改
+
+        //设置新的状态
+        pre = cur; // 移动下一个指向的变量
+        cur = ne; // 保存的下一个节点会变成新的节点
+    }
+
+    head->next = NULL;
+    head = pre;
+
+    return head;
+}
+
+/*
+
+ struct Node* reverseLikedList(struct Node *head) {
     struct Node *tail;
 
+    struct Node *tempNode = head;
+    struct Node *nextNode = tempNode->next;
+    struct Node *nextNextNode;
+    tempNode->next = NULL; // 第一个变成最后一个后next需要置空
+
+    int i = 0;
+    while (nextNode != NULL) {
+        i++;
+        nextNextNode = nextNode->next; // may be null
+        if (nextNextNode == NULL) {
+            tail = nextNode;
+        }
+
+        nextNode->next = tempNode;
+
+        tempNode = nextNode;
+        nextNode = nextNextNode;
+        if (nextNode->next != NULL) {
+            LOGD("node nextNode->next = %d", nextNode->next->id);
+        } else {
+            LOGD("node nextNode->next = null");
+        }
+        LOGE("node nextNode %d = %d", i, nextNode->id);
+    }
 
 
     return tail;
 }
+
+ */
